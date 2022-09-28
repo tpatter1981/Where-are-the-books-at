324f799fba90c0d46f324ba0bc7f443ef19d8106
Tiffani Patterson <71092406+tpatter1981@users.noreply.github.com>
2022-09-28 16:38:45 -0400

    this is my second commit

--- Main/main.js
+++ Main/main.js
@@ -1,23 +1,23 @@
 // Accordion
 function myFunction(id) {
     var x = document.getElementById(id);
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-      x.previousElementSibling.className += " w3-theme-d1";
+    if (x.className.indexOf("wh-show") == -1) {
+      x.className += " wh-show";
+      x.previousElementSibling.className += " wh-theme-d1";
     } else { 
-      x.className = x.className.replace("w3-show", "");
+      x.className = x.className.replace("wh-show", "");
       x.previousElementSibling.className = 
-      x.previousElementSibling.className.replace(" w3-theme-d1", "");
+      x.previousElementSibling.className.replace(" wh-theme-d1", "");
     }
   }
   
   // Used to toggle the menu on smaller screens when clicking on the menu button
   function openNav() {
-    var x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
+    var x = document.getElementById("navBar");
+    if (x.className.indexOf("wh-show") == -1) {
+      x.className += " wh-show";
     } else { 
-      x.className = x.className.replace(" w3-show", "");
+      x.className = x.className.replace(" wh-show", "");
     }
   }
  
--- login/login.js
+++ login/login.js
@@ -2,3 +2,14 @@ const mysql = require('mysql');
 const express = require('express');
 const session = require('express-session');
 const path = require('path');
+
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,2000);
+
+function nextSlide() {
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
+
